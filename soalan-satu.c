7f15b6528b7fdcfaba28dcfdeb898810129755b5
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2022-01-02 00:22:22 +0800

    Create soalan-satu.c

---
+++ soalan-satu.c
@@ -0,0 +1,19 @@
+ if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_5) == 1){
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 1);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
+	}
+	if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_5) == 0) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 0);
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+	}
+
+	if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6) == 1){
+	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 1);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 1);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 1);
+		}
+	if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6) == 0) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 0);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 0);
+	}
