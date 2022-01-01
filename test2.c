ca86f1c42998c1ddfb096a1d979d8b24eb7806f8
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2022-01-01 23:38:35 +0800

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,4 +1,4 @@
-if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_5) != (uint32_t)GPIO_PIN_RESET){
+if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_5) == 1){
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 1);
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
 	}
@@ -7,7 +7,7 @@ if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_5) != (uint32_t)GPIO_PIN_RESET){
 	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
 	}
 	
-	if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6) != (uint32_t)GPIO_PIN_RESET){
+	if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6) == 1){
 	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 1);
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 1);
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 1);
